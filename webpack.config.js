12523c0483c0cdf1ef48fdb5a35c64969f638764
Eric Huet <ewhuet@gmail.com>
2024-04-16 10:59:15 +0300

    setup the webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'To Do',
+      icon: "./src/styles/magnify.svg",
+      filename: "index.html",
+      template: path.resolve(__dirname, "src", "index.html"),
+      inject: true,
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/inline',
+      },
+    ],
+  },
+};
